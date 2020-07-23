5c3fa12584a428105c0a76b5897b59349b5114cf
Alexander Maslennikov <amaslenn@gmail.com>
2020-07-23 13:18:43 +0200

    Added prepare script

---
+++ use-cases/README.md
@@ -0,0 +1,3 @@
+# Use-cases
+
+Use `prepare.sh` to copy `modules` folder containing generic SODALITE types into use-cases folders 
---
+++ use-cases/prepare.sh
@@ -0,0 +1,5 @@
+rm -r -f vehicleiot-uc/full-demo/modules/
+cp -r modules vehicleiot-uc/full-demo/
+
+rm -r -f snow-uc/snow-demo/modules
+cp -r modules snow-uc/snow-demo/
