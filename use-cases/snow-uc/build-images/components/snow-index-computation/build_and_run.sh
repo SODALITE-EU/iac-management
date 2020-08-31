591a7c4faca65f136ac6804ce5b92bbab9f6166a
Gianmarcodpc <gianmarco.dello@mail.polimi.it>
2020-08-31 12:28:55 +0200

    updated dockerfiles after testing all components

--- use-cases/snow-uc/build-images/components/snow-index-computation/build_and_run.sh
+++ use-cases/snow-uc/build-images/components/snow-index-computation/build_and_run.sh
@@ -2,4 +2,4 @@ cd "${0%/*}" || exit
 sudo docker build -t snow-snow-index-computation .
 mkdir -p Builds/
 sudo docker save -o Builds/snow-snow-index-computation.tar snow-snow-index-computation
-sudo docker run -v "$PWD/Builds":/home/snow-snow-index-computation/build/ -it snow-snow-index-computation
+sudo docker run -p 5005:5005 -v "$PWD/Builds":/home/snow-snow-index-computation/build/ -it snow-snow-index-computation
--- use-cases/snow-uc/build-images/components/snowwatch-render/build_and_run.sh
+++ use-cases/snow-uc/build-images/components/snowwatch-render/build_and_run.sh
@@ -2,4 +2,4 @@ cd "${0%/*}" || exit
 sudo docker build -t snow-snowwatch-render .
 mkdir -p Builds/
 sudo docker save -o Builds/snow-snowwatch-render.tar snow-snowwatch-render
-sudo docker run -v "$PWD/Builds":/home/snow-snowwatch-render/build/ -it snow-snowwatch-render
+sudo docker run -p 3000:3000 -v "$PWD/Builds":/home/snow-snowwatch-render/build/ -it snow-snowwatch-render
